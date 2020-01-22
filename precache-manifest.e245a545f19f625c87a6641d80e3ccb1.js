7a5f82dcbc863330f8a7aa9ff93df5b59630af23
jumpwmk <methus159357@gmail.com>
2020-01-22 15:37:18 +0700

    Updates

---
+++ precache-manifest.e245a545f19f625c87a6641d80e3ccb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10740ca8fc9d3db776ad1ac4f0e8c198",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c872851c6138f26d6aa1",
+    "url": "/monster-rolodex/static/css/main.fd5d4f1e.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/monster-rolodex/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c872851c6138f26d6aa1",
+    "url": "/monster-rolodex/static/js/main.2d10868c.chunk.js"
+  },
+  {
+    "revision": "939c7cac93c69c649cbc",
+    "url": "/monster-rolodex/static/js/runtime-main.11039a90.js"
+  }
+]);
